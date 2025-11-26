1980a7da1254f88474e01dbad0b1d40fa5f3eaa0
Valeriy <student174471@gmail.com>
2025-11-26 19:49:33 +0300

    feat: вывод произведения A * B

--- lab/main.cpp
+++ lab/main.cpp
@@ -10,7 +10,8 @@ int main()
     int a, b;
     cout << "Enter A and B: ";
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n';
-    cout << "A - B = " << a - b << '\n';
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n';
     return 0;
 }
